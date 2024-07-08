3bb83505868b79ff28bb8a738a65bdcd1b7f8690
jessi <jessicasalmeida@hotmail.com>
2024-07-07 21:43:38 -0300

    configure to aws

--- src/external/data-sources/postgresql/db-config.ts
+++ src/external/data-sources/postgresql/db-config.ts
@@ -4,18 +4,23 @@ import { CartEntity } from '../../../core/entities/cart';
 import { UserEntity } from '../../../core/entities/user';
 import { CartItemEntity } from '../../../core/entities/cart-item';
 
+const fs = require('fs')
 const config: DataSourceOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: 'terraform-20240708001122858300000001.crviqyiohm3x.us-east-1.rds.amazonaws.com',
   port: 5432,
   username: 'postgres',
-  password: '1234',
-  database: 'postgres',
+  password: 'fiapfase4!',
+  database: 'cart-db',
   synchronize: true,
   logging: false,
   entities: [ProductEntity, CartEntity, UserEntity, CartItemEntity],
   migrations: [],
   subscribers: [],
+  extra: {
+    rejectUnauthorized: true,
+    ca: fs.readFileSync('global-bundle.pem').toString(),
+  }
 };
 
 export default config;
